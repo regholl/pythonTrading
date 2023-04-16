c79b87e3286c2b88163452cf26033834b875c387
dBCooper2 <t.rowland711@gmail.com>
2023-04-16 15:47:49 -0500

    made abstract and started files, tda-api doesnt work atm so i will build one based on a new api soon

---
+++ code/api_connection.py
@@ -0,0 +1,7 @@
+# api_connection.py
+#
+# Author: Trevor Rowland
+#
+#	A Program to connect to the TDAmeritrade API and the tda-api to access portfolio and 
+# stock data
+
---
+++ code/stock.py
@@ -0,0 +1,16 @@
+# Author: Trevor Rowland
+#
+#	A Class to define a stock object, stores name, price and other relevant info.
+class Stock:
+	def __init__(self, name):
+		name = self.name
+		# get price data from API
+		
+	def get_name(self):
+		return self.name
+		
+	def get_price(self):
+		#return the price from API
+		return 0
+		
+	# defs to get stats
