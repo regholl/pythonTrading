3fb2bc8db58b69cd453f20aa9267a2d5b58b983f
dBCooper2 <t.rowland711@gmail.com>
2023-04-28 11:49:34 -0500

    chatGPT wrote sql file

--- code/frontend/app.py
+++ code/frontend/app.py
@@ -13,18 +13,22 @@ def home():
 def ui_nav_buttons_hp(): # UI Navigation Buttons Homepage
     if request.method == 'POST': 
         if request.form.get('positions') == 'Positions': # Positions Button
-            print('a')
+            print('position_button')
             return render_template('positions.html')
         elif request.form.get('orders') == 'Orders': # Orders Button
-            print('2')
+            print('order_button')
             return render_template('orders.html')
         elif request.form.get('settings') == 'Settings':
+            print('settings_button')
             return render_template('settings.html')
         else:
             print('step in at line 16')
             pass
     else:
         return render_template('home.html')
+    
+@app.route('/<pathname>/<id>', methods = ['POST', 'GET'])
+def get_by_id():
 
 
 if __name__ == '__main__': # If this is the main method
